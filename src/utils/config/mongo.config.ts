617c93fd673410790eaf2df8f04f511c2e3ef44b
glumsik <glumsik@icloud.com>
2024-05-21 02:26:22 +0300

    add compose nest

--- src/utils/config/mongo.config.ts
+++ src/utils/config/mongo.config.ts
@@ -1,10 +1,10 @@
-const { DB_USERNAME, DB_PASSWORD, DB_PORT } = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_PORT, MONGO_HOST } = process.env;
 
-const MONGO_HOST = '127.0.0.1';
+const mongoHost = MONGO_HOST || '127.0.0.1';
 
 const UserDB = DB_USERNAME || 'purpleschool';
 const PasswordDB = DB_PASSWORD || 'purpleschool123';
 const Port = DB_PORT || 47018;
 
 export const getUri = () =>
-	`mongodb://${UserDB}:${PasswordDB}@${MONGO_HOST}:${Port}?directConnection=true`;
+	`mongodb://${UserDB}:${PasswordDB}@${mongoHost}:${Port}?directConnection=true`;
